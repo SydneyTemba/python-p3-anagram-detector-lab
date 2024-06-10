d61ee0e5140182513e386752d12a14eab37926f7
Temba Sydney Gabriel <tembasydney@gmail.com>
2024-06-10 12:25:06 +0300

    assignment done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagram = []
+        for word in words:
+            if sorted(word) == sorted(self.word) and word!= self.word:
+                anagram.append(word)
+        return anagram
